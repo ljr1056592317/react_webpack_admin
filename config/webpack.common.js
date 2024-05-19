83c9cdb77f64148dfc24b2cb7702e9affddf2e47
ljr1056592317 <1056592317@qq.com>
2024-05-19 22:46:36 +0800

    feat: 添加项目对静态资源的处理

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -83,6 +83,31 @@ module.exports = {
           },
         ],
       },
+      {
+        //字体
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg|webp|svg)$/,
+        type: 'asset',
+        parser: {
+          // 当提供函数时，返回 true 值时告知 webpack 将模块作为一个 Base64 编码的字符串注入到包中，
+          // 否则模块文件会被生成到输出的目标目录中。将base64的资源都放在一个目录下
+          dataUrlCondition: (source, { filename }) => {
+            // 1. 如果是base64下的目录，将文件打包成base64
+            if (filename.includes('assets/base64_images')) {
+              return true
+            }
+            // 3. 对于小于imageInlineSizeLimit的文件，会以Base64 编码的字符串注入到包中
+            if (source.length <= 4 * 1024) {
+              return true
+            }
+            return false
+          },
+        },
+        exclude: /node_modules/, //排除 node_modules 目录
+      },
     ],
   },
 }
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 import './app.less'
+import image from '../public/assets/lockScreen.jpg'
 const root = document.querySelector('#root')
 root.innerHTML = '你好啊，这是wepac5k搭建的项目--改变10'
