d651c60b80f340faacd732a1958dea778bde795d
ljr1056592317 <1056592317@qq.com>
2024-05-19 21:09:26 +0800

    feat: 添加清楚打包后清理dist文件夹的插件

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,13 +1,18 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const PLUGINS = [new CleanWebpackPlugin()]
+
 const proWebpackConfig = merge(common, {
   mode: 'production',
-  devtool: 'none',
+  devtool: false,
   output: {
     filename: '[name].[contenthash:8].js',
     publicPath: '/',
     chunkFilename: '[name][id].[chunkhash].js',
     assetModuleFilename: 'images/[hash][ext][query]',
   },
+  plugins: PLUGINS,
 })
 module.exports = proWebpackConfig
--- src/app.js
+++ src/app.js
@@ -1,2 +1,2 @@
 const root = document.querySelector('#root')
-root.innerHTML = '你好啊，这是wepac5k搭建的项目--改变'
+root.innerHTML = '你好啊，这是wepac5k搭建的项目--改变2'
