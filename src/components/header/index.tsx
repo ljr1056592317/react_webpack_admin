75475eccd1fd92d2990abe83fc8f4a08c624c3a8
ljr1056592317 <1056592317@qq.com>
2024-05-20 18:59:13 +0800

    feat: babel添加es6+转成es5的处理

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,8 @@
 import styles from './index.module.less'
-
+const testFn = (a: number, b: number) => {
+  return a + b
+}
+testFn(1, 2)
 const Header = () => {
   return <div className={styles.header}>我的头部</div>
 }
