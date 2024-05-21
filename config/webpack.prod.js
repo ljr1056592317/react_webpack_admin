32a622a39fc01b22142600c8387c7bccfc042aaf
ljr1056592317 <1056592317@qq.com>
2024-05-22 00:14:24 +0800

    feat: 本地打包开启gzip压缩

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,6 +5,8 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer') // 使用交
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 抽离css文件, 这个插件将CSS取到单独的文件中。它为每个包含CSS的JS文件创建一个CSS文件。它支持按需加载 CSS 和 SourceMaps。
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin') // 这个插件优化和压缩css
 const TerserPlugin = require('terser-webpack-plugin') // 这个插件使用 terser 来压缩 JavaScript，可以多进程压缩，删除注释、去除console
+const CompressionPlugin = require('compression-webpack-plugin') // 静态资源压缩, 使用Content-Encoding为它们提供服务
+
 const { shouldOpenAnalyzer } = require('../src/utils/envConstans.js')
 
 const PLUGINS = [
@@ -15,6 +17,12 @@ const PLUGINS = [
     filename: 'css/[name].[contenthash:8].css',
     chunkFilename: 'css/[name].[contenthash:8].chunk.css',
     ignoreOrder: true,
+  }),
+  new CompressionPlugin({
+    test: /\.js$|\.html$|.\css/, // 匹配文件名,一般来说css不会很大，在压成.gz没有啥意义
+    threshold: 10240, // 对超过10k的数据压缩
+    deleteOriginalAssets: true, // 不删除源文件，如果删除也源文件，那么当用户出现访问.gz文件访问不到，还可以访问源文件，双重保障
+    // minRatio: 0.8, // 压缩比
   }),
 ].filter(Boolean)
 
