5b3f2bf9ff622c227254b736cde6c94813cc8a89
ljr1056592317 <1056592317@qq.com>
2024-06-07 11:10:17 +0800

    feat: 更新404路由

--- src/page/NotFound/index.tsx
+++ src/page/NotFound/index.tsx
@@ -1,5 +1,27 @@
-function NotFound() {
-  return <div className="not-found-root">404</div>
+import { Button, Result } from 'antd'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const NoFoundPage: React.FC = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="您貌似走丢了.."
+      extra={
+        <Button
+          type="primary"
+          onClick={() => {
+            navigate('/')
+          }}
+        >
+          回到主页
+        </Button>
+      }
+    />
+  )
 }
 
-export default NotFound
+export default NoFoundPage
